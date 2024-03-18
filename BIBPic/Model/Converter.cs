5ee36165bbb5e5d246be87351d1fe4f435786b79
Dennis Razlaw <DRazlaw@dspace.de>
2024-03-18 12:03:57 +0100

    Structure Converter

---
+++ BIBPic/Model/Converter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Image = System.Drawing.Image;
+
+namespace BIBPic.Model
+{
+    internal class Converter
+    {
+        
+        public static Image ReSizeImage(Image imageToResize, Size size)
+        {
+            int sourceWidth = (int)imageToResize.Width;
+            int sourceHeight = (int)imageToResize.Height;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            System.Drawing.Bitmap b = new System.Drawing.Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((Image)b);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+            g.DrawImage(imageToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+
+            return (Image)b;
+
+        }
+
+    }
+}
---
+++ BIBPic/Model/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+using Serilog.Events;
+
+namespace BIBPic.Model
+{
+    internal class Logger
+    {
+                        private static ILogger _logger;
+        public static void InitializeLogger()
+        {
+            _logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+        }
+        public static void Log(string message, LogEventLevel level)
+        {
+            _logger.Write(level, message);
+        }
+    
+
+    }
+}
