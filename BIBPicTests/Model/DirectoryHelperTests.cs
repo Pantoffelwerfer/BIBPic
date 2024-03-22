113a19775abecf3fd69e043481cda9517f21c890
Dennis Razlaw <DRazlaw@dspace.de>
2024-03-22 18:35:15 +0100

    Added Test for DirectoryHelper

---
+++ BIBPicTests/Model/DirectoryHelperTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BIBPic.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BIBPic.Ressources;
+
+namespace BIBPic.Model.Tests
+{
+    [TestClass()]
+    public class DirectoryHelperTests
+    {
+        private DirectoryHelper _directoryHelper = new();
+
+
+
+        [TestMethod()]
+        public void GetClassNamesFromExcel_WhenExcelFileExists_ReturnsClassNames()
+        {
+            // Arrange
+
+            // Act
+            List<ClassNames> result = _directoryHelper.GetClassNamesFromExcel();
+
+            // Assert
+            Assert.IsNotNull(result);
+            // Add more assertions as needed
+        }
+
+        [TestMethod()]
+        public void GetClassNamesFromExcel_ExcelFileContainsMoreThanOneName_ReturnsTrue()
+        {
+            // Arrange
+
+            // Act
+            List<ClassNames> result = _directoryHelper.GetClassNamesFromExcel();
+
+            // Assert
+            Assert.IsTrue(result.Count >= 0);
+            // Add more assertions as needed
+        }
+
+    }
+
+}
